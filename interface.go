7b7721613677bd1e5367faba60d686342d235ec5
Henrique Rodrigues <henrique.93.rodrigues@gmail.com>
2018-10-16 19:38:41 -0300

    update on interface

--- interface.go
+++ interface.go
@@ -8,7 +8,7 @@ import (
 // Interface contains the methods of this lib
 type Interface interface {
 	RegisterPlayer(ctx context.Context, user string, password []byte) error
-	Authorize(ctx context.Context, user, room string) error
-	AuthorizeWithExpire(ctx context.Context, user, room string, expiresAt time.Time) error
-	Unauthorize(ctx context.Context, user, room string) error
+	Authorize(ctx context.Context, user string, rooms []string) error
+	AuthorizeWithExpire(ctx context.Context, user string, room []string, expiresAt time.Time) error
+	Unauthorize(ctx context.Context, user string, room []string) error
 }
